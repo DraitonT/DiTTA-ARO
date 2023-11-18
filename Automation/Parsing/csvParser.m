f729722f536a2034fe885d1fa9d39d7e3c3c1e59
DraitonT <mdquach@cpp.edu>
2023-11-17 21:29:15 -0800

    csvParser.m completed and outputs all interpolated results for the desired locations

--- Automation/Parsing/csvParser.m
+++ Automation/Parsing/csvParser.m
@@ -8,22 +8,30 @@ tic
 % runs of specific cut
 % other .m files required: None
 % other files required (not .m): Folder with the individual results
+
 %%%%%%%%%%%%%%%%%%%%%%%%% EDITABLE %%%%%%%%%%%%%%%%%%%%%%%%%
-%% 0.0 Location of Cut and folder of interest
+%% 0.0 Location of all data files
 folderOfInterest = '\..\..\data\'; 
-
 num_nearest_nodes = 4; % Set the number of nearest nodes to use for interpolation
+
 %%%%%%%%%%%%%%%%%%%%%%%%% EDITABLE %%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%%%%%%%%%%%%%%%%%%%%%% NON-EDITABLE %%%%%%%%%%%%%%%%%%%%%%%
 %% Initialize an empty table to store all interpolated values
 all_interpolated_data = table();
 
-%% 1.0 IDW Interpolation
-% Define user coordinates
-user_x = 1; % Replace with actual value or user input
-user_y = 1; % Replace with actual value or user input
-user_z = 1; % Replace with actual value or user input
+%% Define an array of user coordinates
+user_coordinates = [
+    5.1, 6, 0.05;
+    7.1, 6, 0.05;
+    9.2, 6, 0.05;
+    11.1, 6, 0.05;
+    15.2, 6, 0.05;
+    17.1, 6, 0.05;
+    21.2, 6, 0.05
+];
 
+%% 1.0 IDW Interpolation
 % Load the data from CSV
 filePath = append(pwd, folderOfInterest);
 folder = dir(filePath);
@@ -36,43 +44,47 @@ for k = 3:length(folder)
 
     csvFileOfInterest = append(filePath, folderName, '\', folderName, 'compiled.csv');
     data = readtable(csvFileOfInterest, 'VariableNamingRule','preserve'); 
-    
+
     % Remove 'Node Numbers' column if it exists
     if any(strcmp('Node Numbers', data.Properties.VariableNames))
-        data(:, 'Node Numbers') = [];
+        data.('Node Numbers') = [];
     end
 
-    % Ensure we have the correct number of columns for interpolation (excluding X, Y, Z locations)
-    numColumnsToInterpolate = width(data) - 3;
+    % Loop through each set of user input coordinates
+    for coord_idx = 1:size(user_coordinates, 1)
+        user_x = user_coordinates(coord_idx, 1);
+        user_y = user_coordinates(coord_idx, 2);
+        user_z = user_coordinates(coord_idx, 3);
 
-    % Calculate the Euclidean distance from each node to the user specified point
-    distances = sqrt((data{:, 'X Locations (inches)'} - user_x).^2 + ...
-                     (data{:, 'Y Locations (inches)'} - user_y).^2 + ...
-                     (data{:, 'Z Locations (inches)'} - user_z).^2);
-    
-    % Find the indices of the nearest nodes
-    [sortedDistances, sortedIndices] = sort(distances);
-    nearestIndices = sortedIndices(1:num_nearest_nodes);
-    nearestDistances = sortedDistances(1:num_nearest_nodes);
-    
-    % Perform IDW interpolation for each data column
-    weights = 1 ./ nearestDistances;
-    normalized_weights = weights / sum(weights);
-    
-    % Preallocate interpolated values vector
-    interpolated_values = zeros(1, numColumnsToInterpolate); 
-    
-    % Interpolate values
-    for j = 1:num_nearest_nodes
-        interpolated_values = interpolated_values + normalized_weights(j) * table2array(data(nearestIndices(j), 4:end));
+        % Calculate the Euclidean distance from each node to the user specified point
+        distances = sqrt((data.('X Locations (inches)') - user_x).^2 + ...
+                         (data.('Y Locations (inches)') - user_y).^2 + ...
+                         (data.('Z Locations (inches)') - user_z).^2);
+
+        % Find the indices of the nearest nodes
+        [sortedDistances, sortedIndices] = sort(distances);
+        nearestIndices = sortedIndices(1:num_nearest_nodes);
+        nearestDistances = sortedDistances(1:num_nearest_nodes);
+
+        % Perform IDW interpolation for each data column
+        weights = 1 ./ nearestDistances;
+        normalized_weights = weights / sum(weights);
+
+        % Preallocate interpolated values vector
+        interpolated_values = zeros(1, width(data) - 3); % Adjust the size based on the number of columns to interpolate
+
+        % Interpolate values
+        for j = 1:num_nearest_nodes
+            interpolated_values = interpolated_values + normalized_weights(j) * table2array(data(nearestIndices(j), 4:end));
+        end
+
+        % Create a new table for the interpolated point
+        interpolated_row = array2table([cutLocation, user_x, user_y, user_z, interpolated_values], ...
+                                       'VariableNames', ['CutLocation', 'X', 'Y', 'Z', data.Properties.VariableNames(4:end)]);
+
+        % Append the interpolated_row to the all_interpolated_data table
+        all_interpolated_data = [all_interpolated_data; interpolated_row];
     end
-
-    % Create a new table for the interpolated point
-    interpolated_row = array2table([cutLocation, user_x, user_y, user_z, interpolated_values], ...
-                                   'VariableNames', ['CutLocation', data.Properties.VariableNames(1:end)]);
-    
-    % Append the interpolated_row to the all_interpolated_data table
-    all_interpolated_data = [all_interpolated_data; interpolated_row];
 end
 
 %% 2.0 Write the results to an Excel file
@@ -81,5 +93,5 @@ writetable(all_interpolated_data, outputFileName);  % Write the table to an Exce
 
 disp(['All interpolated values have been written to ', outputFileName]);
 
-
+%%%%%%%%%%%%%%%%%%%%%%% NON-EDITABLE %%%%%%%%%%%%%%%%%%%%%%%
 toc

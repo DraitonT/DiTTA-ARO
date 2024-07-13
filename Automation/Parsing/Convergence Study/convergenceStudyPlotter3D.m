7d4186f169dc7e63e34c3a46492ce8cce98c7150
DraitonT <mdquach@cpp.edu>
2024-07-13 12:55:07 -0700

    Missing files, incorrect name on GitHub

---
+++ Automation/Parsing/Convergence Study/convergenceStudyPlotter3D.m
@@ -0,0 +1,21 @@
+clc, clear, close all
+addpath(genpath(append(pwd,'\..\..\lib'))) % Adds TwinAnalyticsToolKit.m library to the working path
+tic
+
+% -------------MATLAB Script Information-------------
+% Author Names: Michael Quach
+% Date: 12/16/23
+% Tool Version: R2023a
+% Purpose of Script: Convergence Study Values combined into seperate CSVs
+% other .m files required: None
+% other files required (not .m): Folder with the individual results
+
+%%%%%%%%%%%%%%%%%%%%%%%%% EDITABLE %%%%%%%%%%%%%%%%%%%%%%%%%
+% Load the combined data from the CSV file
+combinedCSVFilePath = append(pwd,'\..\..\data\convergenceStudy\combinedData.csv'); 
+
+%%%%%%%%%%%%%%%%%%%%%%% NON-EDITABLE %%%%%%%%%%%%%%%%%%%%%%%
+
+TwinAnalyticsToolKit.ThreeDConvergencePlotter(combinedCSVFilePath)
+
+%%%%%%%%%%%%%%%%%%%%%%% NON-EDITABLE %%%%%%%%%%%%%%%%%%%%%%%
